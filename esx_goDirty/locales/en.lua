aa8c0c05837146c6a1f730b824aa666786e4f841
Krizfrost <dejkriz@outlook.com>
2018-03-14 16:40:26 -0400

    Unpacked Zip

---
+++ esx_goDirty/locales/en.lua
@@ -0,0 +1,39 @@
+Locales['en'] = {
+	['cloakroom']				= 'cloakroom',
+	['job_wear']				= 'workwear',
+	['citizen_wear']			= 'civilian clothes',
+	['vehiclespawner']			= 'choice of truck', 
+	['already_have_truck']		= 'we have allready provided a truck!', 
+	['delivery']				= 'press ~INPUT_PICKUP~ to deliver.',
+	['not_your_truck']			= 'it is not the truck that you have provided !',
+	['not_your_truck2']			= 'you must be in the truck that you were provided with !',
+	['need_it']					= 'yeah, but we need it !',
+	['ok_work']					= 'ok, at work then !',
+	['scared_me']				= 'ok, you scared me there !',
+	['resume_delivery']			= 'ok, resume your delivery then !',
+	['no_delivery']				= 'no delivery no Cash',
+	['pay_repair']				= 'on the other hand you pay the repairs !',
+	['repair_minus']			= 'truck repairs : -',
+	['shipments_plus']			= 'shipments : +',
+	['truck_state']				= 'no money dude, seen the state fo the truck !',
+	['no_delivery_no_truck']	= 'no delivery, no truck! Its a joke?',
+	['truck_price']				= 'truck price : -',
+	['meet_ls']					= 'go to the delivery point at Los Santos',
+	['meet_bc']					= 'go to the delivery point at Blaine County',
+	['meet_del']				= 'go to the delivery point',
+	['return_depot']			= 'return to the depot.',
+	['blip_job']				= 'Post Office',
+	['blip_delivery']			= 'Post Office : Delivery',
+	['blip_goal']				= 'Post Office : Delivery point',
+	['blip_depot']				= 'Post Office : depot',
+	['cancel_mission']			= 'Press ~INPUT_PICKUP~	to ~r~Return~w~ Truck',
+	['not_delivery']			= '~r~You did not do any deliveries',
+	['Money_Laundering']		= 'Money Laundering'
+}
+
+--------------------------------------------------------------------------
+------------------------------BY  K R I Z F R O S T-----------------------
+--------------------------------------------------------------------------
+----- MONEY LAUNDERING SCRIPT RELEASED CREDITS TO ORIGINAL CREATOR--------
+----- OF ESX_POSTALJOB RE WORKED AND MODIFIED BY KRIZFROST ---------------
+--------------------------------------------------------------------------
