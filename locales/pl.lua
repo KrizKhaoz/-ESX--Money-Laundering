27a4b35d957410df2a66c1bdd0c33525699ed5aa
KubSztajn <43608386+KubSztajn@users.noreply.github.com>
2019-02-10 16:37:58 +0100

    pl.lua
    Polish translation

---
+++ locales/pl.lua
@@ -0,0 +1,41 @@
+Locales['en'] = {
+	['cloakroom']				= 'szatnia',
+	['job_wear']				= 'Załaduj pojazd [max $5000 Brudnych Pieniędzy]',
+	['citizen_wear']			= 'ubrania cywilne',
+	['vehiclespawner']			= 'wybór pojazdu', 
+	['already_have_truck']		= 'już zapewniliśmy ci ciężarówkę!', 
+	['delivery']				= 'naciśnij ~INPUT_PICKUP~ aby dostarczyć.',
+	['not_your_truck']			= 'to nie jest ciężarówka, którą od nas otrzymałeś !',
+	['not_your_truck2']			= 'musisz być w ciężarówce, którą od nas otrzymałeś !',
+	['need_it']					= 'tak, ale potrzebujemy tego !',
+	['ok_work']					= 'ok, pracuj !',
+	['scared_me']				= 'ok, przestraszyłeś mnie !',
+	['resume_delivery']			= 'ok, kontynuuj swoją dostawę !',
+	['no_delivery']				= 'brak dostawy, brak pieniędzy',
+	['pay_repair']				= 'płacisz za uszkodzenia !',
+	['repair_minus']			= 'naprawa pojazdu : -',
+	['shipments_plus']			= 'dostawy : +',
+	['truck_state']				= 'brak pieniędzy, zobacz stan ciężarówki !',
+	['no_delivery_no_truck']	= 'brak dostawy, brak ciężarówki. Czy to żart?',
+	['truck_price']				= 'koszt ciężarówki : -',
+	['meet_ls']					= 'jedź do punktu dostawy w Los Santos',
+	['meet_bc']					= 'jedź do punktu dostawy w Blaine County',
+	['meet_del']				= 'jedź do punktu dostawy',
+	['return_depot']			= 'wracaj do bazy.',
+	['blip_job']				= 'Poczta',
+	['blip_delivery']			= 'Poczta : Delivery',
+	['blip_goal']				= 'Poczta : Delivery punkt',
+	['blip_depot']				= 'Poczta : magazyn',
+	['cancel_mission']			= 'Naciśnij ~INPUT_PICKUP~	aby ~r~zwrócić~w~ Ciężarówkę',
+	['not_delivery']			= '~r~Nie zrobiłeś żadnej dostawy!',
+	['Money_Laundering']		= Pranie Pieniędzy',
+	['return_finished']			= 'Naciśnij ~INPUT_PICKUP~ aby wylogować się i zgarnąć ~g~Czyste Pieniądze'
+}
+
+--------------------------------------------------------------------------
+------------------------------BY  K R I Z F R O S T-----------------------
+--------------------------------------------------------------------------
+----- MONEY LAUNDERING SCRIPT RELEASED CREDITS TO ORIGINAL CREATOR--------
+----- OF ESX_POSTALJOB RE WORKED AND MODIFIED BY KRIZFROST ---------------
+--------------------------------------------------------------------------
+-----------------------------TRANSLATED BY KUBSZTAJN----------------------
